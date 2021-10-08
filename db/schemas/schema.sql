fd3e8380ee644732decccdc519838b76dcb4ebdc
Fikcup <rhys.n.wright@gmail.com>
2021-10-08 12:19:28 -0700

    feat : added user and books tables and seeds

---
+++ db/schemas/schema.sql
@@ -0,0 +1,30 @@
+--CREATE DATABASE--
+DROP DATABASE IF EXISTS bookList;
+
+CREATE DATABASE bookList;
+
+USE bookList;
+
+--USERS TABLE--
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(25) NOT NULL UNIQUE,
+    userPassword VARCHAR(75) NOT NULL,
+    firstName VARCHAR(30) NOT NULL,
+    lastName VARCHAR(30),
+);
+
+--BOOKS TABLE--
+CREATE TABLE books (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    author VARCHAR(50) NOT NULL,
+);
+
+-- --LISTS TABLE--
+-- CREATE TABLE readinglist (
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     listName VARCHAR(30) NOT NULL,
+--     user VARCHAR(50) NOT NULL,
+--     FOREIGN KEY (user) REFERENCES users(username)
+-- );
---
+++ db/seeds/books.sql
@@ -0,0 +1,29 @@
+--USERS SEEDS--
+INSERT INTO user ("JackieReads", "b00kw0rm#3", "Jackie", "LeBlanc");
+
+--BOOK SEEDS--
+INSERT INTO books ("The Hunger Games", "Suzanne Collins");
+INSERT INTO books ("Twilight", "Stephanie Meyer");
+INSERT INTO books ("The Book Thief", "Markus Zusak");
+INSERT INTO books ("To Kill a Mockingbird", "Harper Lee");
+INSERT INTO books ("The Chronicles of Narnia", "C.S. Lewis");
+INSERT INTO books ("Gone with the Wind", "Margaret Mitchell");
+INSERT INTO books ("The Giving Tree", "Shel Silverstein");
+INSERT INTO books ("The Picture of Dorian Gray", "Oscar Wilde");
+INSERT INTO books ("The Da Vinci Code", "Dan Brown");
+INSERT INTO books ("Alice's Adventures in Wonderland", "Lewis Carroll");
+INSERT INTO books ("Farenheit 451", "Ray Bradbury");
+INSERT INTO books ("The Perks of Being a Wallflower", "Stephen Chbosky");
+INSERT INTO books ("Divergent", "Veronica Roth");
+INSERT INTO books ("The Great Gatsby", "F. Scott Fitzgerald");
+INSERT INTO books ("Harry Potter and the Sorcerer's Stone", "J.K. Rowling");
+INSERT INTO books ("Lord of the Flies", "William Golding");
+INSERT INTO books ("Crime and Punishment", "Fyodor Dostoevsky");
+INSERT INTO books ("Romeo and Juliet", "William Shakespeare");
+INSERT INTO books ("City of Bones", "Cassandra Clare");
+INSERT INTO books ("Anne of Green Gables", "L.M. Montgomery");
+INSERT INTO books ("Ender's Game", "Orson Scott Card");
+INSERT INTO books ("Charlotte's Web", "E.B. White");
+INSERT INTO books ("The Lightning Thief", "Rick Riordan");
+INSERT INTO books ("The Storied Life of A.J. Fikry", "Gabrielle Zevin");
+INSERT INTO books ("The Catcher in the Rye", "J.D. Salinger");
