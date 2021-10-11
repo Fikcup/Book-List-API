1860f4640d95eda98c3e5545a6a477c91770a653
Fikcup <rhys.n.wright@gmail.com>
2021-10-10 19:26:58 -0700

    feat : get one book

--- controllers/book-controller.js
+++ controllers/book-controller.js
@@ -14,6 +14,20 @@ const bookController = {
                 res.json(req);
             }
         )
+    },
+    getOneBook(req, res) {
+        connection.query(
+            `SELECT * FROM books WHERE id= ${req.params.bookId}`,
+            (err, req) => {
+                if (err) {
+                    console.log(err);
+                    res.status(500).json(err);
+                    return;
+                }
+
+                res.json(req);
+            }
+        )
     }
 }
 
--- routes/api/book-routes.js
+++ routes/api/book-routes.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
 
 const {
-    getAllBooks
+    getAllBooks,
+    getOneBook,
 } = require('../../controllers/book-controller');
 
 // route /api/books
 router.route('/').get(getAllBooks);
+
+// route /api/books/:bookId
+router.route('/:bookId').get(getOneBook);
 
 module.exports = router;
