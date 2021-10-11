a2e6a17dbf8ce97951c58e3ddb847ef233313190
Fikcup <rhys.n.wright@gmail.com>
2021-10-10 21:38:22 -0700

    docs : added documentation to user-controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,7 @@
 const connection = require('../db/config/connection');
 
 const userController = {
+    // Get all users
     getAllUsers(req, res) {
         connection.query(
             `SELECT id, username, firstName, lastName FROM users`,
@@ -15,6 +16,7 @@ const userController = {
             }
         )
     },
+    // Get one user by id
     getOneUser(req, res) {
         connection.query(
             `SELECT * FROM users WHERE id = ${req.params.userId}`,
@@ -29,6 +31,7 @@ const userController = {
             }
         )
     },
+    // Create a new user
     createUser(req, res) {
         connection.query(
             `INSERT INTO users (username, userPassword, firstName, lastName) VALUES (${JSON.stringify(req.body.username)}, ${JSON.stringify(req.body.userPassword)}, ${JSON.stringify(req.body.firstName)}, ${JSON.stringify(req.body.lastName)})`,
@@ -43,6 +46,7 @@ const userController = {
             }
         )
     },
+    // Update a user
     updateUser(req, res) {
         connection.query(
             `UPDATE users SET username = ${JSON.stringify(req.body.username)}, userPassword = ${JSON.stringify(req.body.userPassword)}, firstName = ${JSON.stringify(req.body.firstName)}, lastName = ${JSON.stringify(req.body.lastName)} WHERE id = ${req.params.userId}`,
@@ -57,6 +61,7 @@ const userController = {
             }
         )
     },
+    // Delete a user
     deleteUser(req, res) {
         connection.query(
             `DELETE from users WHERE id = ${req.params.userId}`,
