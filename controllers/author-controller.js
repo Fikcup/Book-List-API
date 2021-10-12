f6e298d4e4bdc94f689f6684dc00b360ec838613
Fikcup <rhys.n.wright@gmail.com>
2021-10-12 10:50:02 -0700

    feat : get all authors

--- controllers/author-controller.js
+++ controllers/author-controller.js
@@ -1,7 +1,20 @@
 const connection = require('../db/config/connection');
 
 const authorController = {
+    getAllAuthors(req, res) {
+        connection.query(
+            `SELECT * FROM authors`,
+            (err, req) => {
+                if (err) {
+                    console.log(err);
+                    res.status(500).json(err);
+                    return;
+                }
 
+                res.json(req);
+            }
+        );
+    }
 }
 
 module.exports = authorController;
--- routes/api/author-routes.js
+++ routes/api/author-routes.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const {
-
+    getAllAuthors,
 } = require('../../controllers/author-controller');
 
 // route /api/author
-router.route('/');
+router.route('/').get(getAllAuthors);
 
 // route /api/author/:authorId
 router.route('/:authorId');
